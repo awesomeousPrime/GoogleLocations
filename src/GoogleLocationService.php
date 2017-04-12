7d12ae61b9cd37ffdf743704937d3dc3fcd84f15
joelpadron <joelpadron@Joels-iMac.local>
2017-04-12 15:44:01 -0500

    testing

---
+++ src/GoogleLocationService.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @todo finish this...
+ * Service class for handling
+ */
+class GoogleLocationService{
+    /**
+     * Testing method.
+     *
+     * @deprecated
+     */
+    public function testing()
+    {
+        echo 'this is the GoogleLocationsService class....';
+    }
+}
