1dbe8093e2cb49b0780c8046f015c04469c47d91
joelpadron <joelpadron@Joels-iMac.local>
2017-04-17 16:30:33 -0500

    adding data files

---
+++ data/indexes/file.php
@@ -0,0 +1,21 @@
+<?php
+
+// Returns the min and max ids in each file.
+return [
+    'google_locations1.json' => [
+        'min' => '1',
+        'max' => '21615',
+    ],
+    'google_locations2.json' => [
+        'min' => '21616',
+        'max' => '43230',
+    ],
+    'google_locations3.json' => [
+        'min' => '43231',
+        'max' => '64845',
+    ],
+    'google_locations4.json' => [
+        'min' => '64846',
+        'max' => '86460',
+    ]
+];
